3683537adbff8e1a90f65e31c8b7af8216953df8
laubosh4-ui <laubosh4@gmail.com>
2026-03-18 16:52:42 +0200

    Create config.dart

---
+++ lib/config.dart
@@ -0,0 +1,9 @@
+class Config {
+  static const String dashboardUrl = 'https://YOUR-APP.base44.app';
+  static const String locationId = 'YOUR_LOCATION_ID';
+  static const String locationName = 'Main Entrance';
+  static const String apiKey = 'YOUR_API_KEY';
+
+  static const double confidenceThreshold = 0.5;
+  static const int personClassId = 0;
+}
